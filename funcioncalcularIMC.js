ddc01bf8e1d6a7ec0346dd8e36bd178935b8e1df
malejalara <malejalara01@gmail.com>
2021-09-03 09:09:29 -0500

    se modifico el js y el html para agregar un document.getElementById('imc').innerHTML que me devuelva el resultado dentro de la calculadora.

--- funcioncalcularIMC.js
+++ funcioncalcularIMC.js
@@ -2,15 +2,20 @@ function calcularIMC(){
     let peso=document.getElementById("peso").value
     let altura=document.getElementById("altura").value
     
-         imc=peso/ Math.pow(altura,2)
-            if (imc < 18.5)
-            alert('Tiene un peso bajo, su IMC es '+Math.round(imc))
-            else if (imc >=18.5 )
-            alert('Tiene un peso normal, su IMC es '+Math.round(imc))
-            else if (imc >=25)
-            alert('Tiene sobre peso, su IMC es '+Math.round(imc))
-            else if(imc >=30)
-            alert('Tiene  obesidad, su IMC es '+Math.round(imc))
-            else if(imc >=40)
-            alert('Tiene obesidad morbida, su IMC '+Math.round(imc))
+        let imc=parseFloat(peso/ Math.pow(altura,2))
+            if (imc < 18.5){
+            document.getElementById('imc').innerHTML='Tiene un peso bajo, su IMC es '+(imc.toFixed(1));
+            } 
+            else if (imc >=18.5 && imc<=24.9){
+            document.getElementById('imc').innerHTML='Tiene un peso normal, su IMC es '+(imc.toFixed(1));
+            }
+            else if (imc >=25 && imc<=29.9){
+            document.getElementById('imc').innerHTML='Tiene sobre peso, su IMC es '+(imc.toFixed(1));
+            }
+            else if(imc >=30 && imc<=39.9){
+            document.getElementById('imc').innerHTML='Tiene  obesidad, su IMC es '+(imc.toFixed(1));
+            }
+            else if(imc >=40){
+            document.getElementById('imc').innerHTML='Tiene obesidad morbida, su IMC '+(imc.toFixed(1));
+            }
 }
